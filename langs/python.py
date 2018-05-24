11408fd3b93ed5400e8b19242d968610aad5b9aa
Christopher A. Flores <Christopher A. Flores>
2018-05-24 10:26:35 -0600

    Added menu to select actions to run

--- langs/python.py
+++ langs/python.py
@@ -24,37 +24,13 @@ class Python(StandardClass):
         self.menu_items = [''] + krunner_settings.get('menu_items', [])
         self.menu_items += ["<args>"]
 
-    def input_args(self, args=None):
-        if args is None:
-            self.window.show_input_panel(
-                "args", str(), self.input_args, None, None
-            )
-            return
-        else:
-            self.command += args.split()
-            Shell(self.window, self.shell, self.command, self.main_file).run()
-
-    def show_menu(self, item_selected=None):
-        """ Recursive function """
-        if item_selected is None:
-            self.window.show_quick_panel(
-                [' '.join([self.main_file, item]) for item in self.menu_items],
-                self.show_menu
-            )
-            return
-        elif item_selected > -1:
-            python = 'python'
-            if self.virtualenv is not None:
-                python = os.path.join(self.virtualenv, python)
-
-            self.command = [python, os.path.join(self.folder, self.main_file)]
-
-            if item_selected > 0 and item_selected != len(self.menu_items) - 1:
-                self.command += [self.menu_items[item_selected]]
-            else:
-                self.input_args()
-                return
-            Shell(self.window, self.shell, self.command, self.main_file).run()
-
     def run(self):
-        self.show_menu()
+        python = 'python'
+        if self.virtualenv is not None:
+            python = os.path.join(self.virtualenv, python)
+
+        self.command = [python, os.path.join(self.folder, self.main_file)]
+
+        shell = Shell(self.window, self.shell, self.command, self.main_file)
+        shell.menu_items = self.menu_items
+        shell.show_menu()
--- utils/shell.py
+++ utils/shell.py
@@ -9,7 +9,7 @@ class Shell:
         self.command = command
         self.main_file = main_file
 
-    def get_shell_command(self):
+    def __get_shell_command(self):
         folder = self.window.extract_variables()['folder']
         if self.shell == 'deepin-terminal':
             return (
@@ -45,8 +45,18 @@ class Shell:
 
         return None
 
-    def run(self):
-        execute = self.get_shell_command()
+    def __input_args(self, args=None):
+        if args is None:
+            self.window.show_input_panel(
+                "args", str(), self.__input_args, None, None
+            )
+            return
+        else:
+            self.command += args.split()
+            self.__run()
+
+    def __run(self):
+        execute = self.__get_shell_command()
 
         if execute is None:
             return
@@ -56,3 +66,19 @@ class Shell:
         self.window.status_message(
             "\tPlease wait... Running %s" % self.main_file
         )
+
+    def show_menu(self, item_selected=None):
+        """ Recursive function """
+        if item_selected is None:
+            self.window.show_quick_panel(
+                [' '.join([self.main_file, item]) for item in self.menu_items],
+                self.show_menu
+            )
+            return
+        elif item_selected > -1:
+            if item_selected > 0 and item_selected != len(self.menu_items) - 1:
+                self.command += [self.menu_items[item_selected]]
+            else:
+                self.__input_args()
+                return
+            self.__run()
